f6897df9ac0f4b113610fce9815198ac4959910b
oshimataiga <161116518+oshimataiga@users.noreply.github.com>
2024-02-27 08:21:18 +0900

    Add files via upload

---
+++ javascript-util/filter-table/index.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", () => {
+  const tables = document.getElementsByClassName("filter-table");
+
+  Array.prototype.forEach.call(tables, (table) => {
+    const filterTable = new FilterTable(table);
+    const filterCtrl = new FilterController(filterTable);
+    filterCtrl.insertFilerBtns();
+  });
+});
+  
+  
