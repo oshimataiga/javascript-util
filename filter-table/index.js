0c5bad7789374e7f5cb9deb0524bbcad54ca053a
taiga <rugbyhanazono02@gmail.com>
2024-03-02 11:30:04 +0900

    最終版

--- filter-table/index.js
+++ filter-table/index.js
@@ -1,10 +1,10 @@
 window.addEventListener("load", () => {
   const tables = document.getElementsByClassName("filter-table");
+
   Array.prototype.forEach.call(tables, (table) => {
     const filterTable = new FilterTable(table);
     const filterCtrl = new FilterController(filterTable);
     filterCtrl.insertFilerBtns();
-    console.log(filterCtrl)
   });
 });
   
